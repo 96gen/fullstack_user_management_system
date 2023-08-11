fb294dcad39283356c10ee300e0c560f0bd35792
96gen <96genJQ@gmail.com>
2023-08-11 10:29:12 +0800

    feat: addUser,getAllUsers,getUserById,updateUser,deleteUser

--- src/main/java/com/gen96/fullstackbackend/Controller/UserController.java
+++ src/main/java/com/gen96/fullstackbackend/Controller/UserController.java
@@ -1,16 +1,15 @@
 package com.gen96.fullstackbackend.Controller;
 
 import com.gen96.fullstackbackend.Entity.User;
+import com.gen96.fullstackbackend.Exception.UserNotFoundException;
 import com.gen96.fullstackbackend.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class UserController {
     @Autowired
     private UserRepository userRepository;
@@ -23,5 +22,31 @@ public class UserController {
     @GetMapping("/users")
     public List<User> getAllUsers(){
         return userRepository.findAll();
+    }
+
+    @GetMapping("/user/{id}")
+    public User getUserById(@PathVariable("id") Long id){
+        return userRepository.findById(id)
+                .orElseThrow(()->new UserNotFoundException(id));
+    }
+
+    @PutMapping("/user/{id}")
+    public User updateUser(@PathVariable("id") Long id, @RequestBody User newUser){
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setName(newUser.getName());
+                    user.setUsername(newUser.getUsername());
+                    user.setEmail(newUser.getEmail());
+                    return userRepository.save(user);
+                }).orElseThrow(()-> new UserNotFoundException(id));
+    }
+
+    @DeleteMapping("/user/{id}")
+    public String deleteUser(@PathVariable("id") Long id){
+        if(!userRepository.existsById(id)){
+            throw new UserNotFoundException(id);
+        }
+        userRepository.deleteById(id);
+        return "User with id " + id + " has been deleted.";
     }
 }
